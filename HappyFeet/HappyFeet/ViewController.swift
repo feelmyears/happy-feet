cc79aa92e5eaec3b347e4751d4717b942d0c5691
Philip Meyers IV <philipmeyers2017@u.northwestern.edu>
2016-02-25 14:46:39 -0600

    Init

---
+++ HappyFeet/HappyFeet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyFeet
+//
+//  Created by Phil Meyers IV on 2/25/16.
+//  Copyright © 2016 happyfeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
